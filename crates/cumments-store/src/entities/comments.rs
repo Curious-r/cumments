67fc0252526418ffe1661c8589224ee224e46efb
Curious <Curious@curious.host>
2026-05-31 20:53:15 +0800

    refactor: modernize database layer for SeaORM 2.0

--- crates/cumments-store/src/entities/comments.rs
+++ crates/cumments-store/src/entities/comments.rs
@@ -4,7 +4,7 @@ use sea_orm::entity::prelude::*;
 #[sea_orm(table_name = "comments")]
 pub struct Model {
     #[sea_orm(primary_key)]
-    pub id: i32,
+    pub id: i64,
     #[sea_orm(unique)]
     pub event_id: String,
     pub room_id: String,
