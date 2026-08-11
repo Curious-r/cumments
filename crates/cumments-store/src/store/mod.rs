fd0fb79e565cb4d28ddbf9d9dd10d54610e68aa9
Curious <Curious@curious.host>
2026-08-11 19:37:51 +0800

    refactor(store): split room registry and site stores
    Move RegistryStore and SiteStore implementations plus the sites model
    conversion into store/registry.rs.

--- crates/cumments-store/src/store/mod.rs
+++ crates/cumments-store/src/store/mod.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use async_trait::async_trait;
 use cumments_core::identity::derive_guest_id_from_public_key;
-use cumments_core::models::{PostSlug, Site, SiteId};
-use cumments_core::ports::{BackfillCursorStore, RegistryStore, SiteStore, VirtualUserStore};
+use cumments_core::models::SiteId;
+use cumments_core::ports::{BackfillCursorStore, VirtualUserStore};
 use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, Set};
 use sea_orm::{ConnectionTrait, DatabaseBackend, Statement};
 use std::path::Path;
@@ -11,6 +11,7 @@ use crate::entities::active_enums::IntentStatus;
 
 pub mod comments;
 pub mod intents;
+pub mod registry;
 
 /// A database-backed implementation of the storage ports.
 #[derive(Clone)]
@@ -180,160 +181,6 @@ impl DbStore {
         .exec(&self.db)
         .await?;
         Ok(())
-    }
-}
-
-#[async_trait]
-impl RegistryStore for DbStore {
-    async fn get_registered_room(
-        &self,
-        site_id: &SiteId,
-        post_slug: &PostSlug,
-    ) -> Result<Option<String>> {
-        let room = room_registry::Entity::find()
-            .filter(room_registry::COLUMN.site_id.eq(site_id.as_str()))
-            .filter(room_registry::COLUMN.post_slug.eq(post_slug.as_str()))
-            .filter(room_registry::COLUMN.is_active.eq(true))
-            .one(&self.db)
-            .await?;
-
-        Ok(room.map(|r| r.room_id))
-    }
-
-    async fn is_room_active(&self, room_id: &str) -> Result<Option<bool>> {
-        let room = room_registry::Entity::find_by_id(room_id.to_owned())
-            .one(&self.db)
-            .await?;
-
-        Ok(room.map(|r| r.is_active))
-    }
-
-    async fn get_registered_room_identity(
-        &self,
-        room_id: &str,
-    ) -> Result<Option<(String, String)>> {
-        let room = room_registry::Entity::find_by_id(room_id.to_owned())
-            .one(&self.db)
-            .await?;
-
-        Ok(room.map(|r| (r.site_id, r.post_slug)))
-    }
-
-    async fn register_room(
-        &self,
-        room_id: &str,
-        site_id: &SiteId,
-        post_slug: &PostSlug,
-    ) -> Result<()> {
-        let active_model = room_registry::ActiveModel {
-            room_id: Set(room_id.to_owned()),
-            site_id: Set(site_id.as_str().to_owned()),
-            post_slug: Set(post_slug.as_str().to_owned()),
-            is_active: Set(true),
-            created_at: Set(chrono::Utc::now()),
-            updated_at: Set(chrono::Utc::now()),
-        };
-
-        room_registry::Entity::insert(active_model)
-            .on_conflict(
-                sea_orm::sea_query::OnConflict::column(room_registry::Column::RoomId)
-                    .update_column(room_registry::Column::IsActive)
-                    .update_column(room_registry::Column::SiteId)
-                    .update_column(room_registry::Column::PostSlug)
-                    .update_column(room_registry::Column::UpdatedAt)
-                    .to_owned(),
-            )
-            .exec(&self.db)
-            .await?;
-
-        Ok(())
-    }
-
-    async fn invalidate_room_registry(&self, room_id: &str) -> Result<()> {
-        room_registry::Entity::update_many()
-            .col_expr(
-                room_registry::Column::IsActive,
-                sea_orm::sea_query::Expr::value(false),
-            )
-            .col_expr(
-                room_registry::Column::UpdatedAt,
-                sea_orm::sea_query::Expr::value(chrono::Utc::now()),
-            )
-            .filter(room_registry::COLUMN.room_id.eq(room_id))
-            .exec(&self.db)
-            .await?;
-        Ok(())
-    }
-}
-
-#[async_trait]
-impl SiteStore for DbStore {
-    async fn get_site(&self, id: &SiteId) -> Result<Option<Site>> {
-        let model = sites::Entity::find_by_id(id.as_str().to_owned())
-            .one(&self.db)
-            .await?;
-
-        Ok(model.map(Site::from))
-    }
-
-    async fn get_site_by_space_id(&self, space_id: &str) -> Result<Option<Site>> {
-        let model = sites::Entity::find()
-            .filter(sites::COLUMN.matrix_space_id.eq(space_id))
-            .one(&self.db)
-            .await?;
-
-        Ok(model.map(Site::from))
-    }
-
-    async fn save_site(&self, site: &Site) -> Result<()> {
-        let active_model = sites::ActiveModel {
-            id: Set(site.id.clone()),
-            matrix_space_id: Set(site.matrix_space_id.clone()),
-            display_name: Set(site.display_name.clone()),
-            created_at: Set(site.created_at),
-        };
-
-        sites::Entity::insert(active_model)
-            .on_conflict(
-                sea_orm::sea_query::OnConflict::column(sites::Column::Id)
-                    .update_columns([sites::Column::MatrixSpaceId, sites::Column::DisplayName])
-                    .to_owned(),
-            )
-            .exec(&self.db)
-            .await?;
-
-        Ok(())
-    }
-
-    async fn ensure_site_exists(&self, site_id: &str, matrix_space_id: &str) -> Result<()> {
-        let active_model = sites::ActiveModel {
-            id: Set(site_id.to_owned()),
-            matrix_space_id: Set(matrix_space_id.to_owned()),
-            display_name: Set(Some(site_id.to_owned())),
-            created_at: Set(chrono::Utc::now()),
-        };
-
-        sites::Entity::insert(active_model)
-            .on_conflict(
-                sea_orm::sea_query::OnConflict::column(sites::Column::Id)
-                    .do_nothing()
-                    .to_owned(),
-            )
-            .exec(&self.db)
-            .await?;
-
-        Ok(())
-    }
-}
-
-impl From<sites::Model> for Site {
-    fn from(model: sites::Model) -> Self {
-        Site {
-            id: model.id,
-            matrix_space_id: model.matrix_space_id,
-            display_name: model.display_name,
-            created_at: model.created_at,
-        }
     }
 }
 
